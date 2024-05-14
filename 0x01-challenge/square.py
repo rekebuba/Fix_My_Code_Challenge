004e77d4acd48bf784896e4f6711c54249ce6c37
rekebuba <abukiabdullahi1@gmail.com>
2024-05-15 00:36:02 +0300

    0x01-challenge

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+""" class for square """
+
 
 class square():
+    """ class gor square """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """ setting args and kwargs """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -13,14 +17,15 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """ parameter of a square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ string format representation """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
