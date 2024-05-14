ae0ca1161e8519f5345b4d5b9eb90d0a0cbda3a4
rekebuba <abukiabdullahi1@gmail.com>
2024-05-15 00:38:15 +0300

    0x01-challenge

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,7 +2,7 @@
 """ class for square """
 
 
-class square():
+class Square():
     """ class gor square """
     width = 0
     height = 0
@@ -16,7 +16,7 @@ class square():
         """ Area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def perimeter_of_my_square(self):
         """ parameter of a square """
         return (self.width * 2) + (self.height * 2)
 
@@ -26,7 +26,7 @@ class square():
 
 
 if __name__ == "__main__":
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
